9d3f01149c74124a9d726cf433ca7274561ccc79
Anton Saenko <anton.s@pyrobyte.ru>
2023-03-18 14:16:20 +0700

    fix: Правка скрипта

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
-const response = fetch("http://jsonplaceholder.typicode.com/posts")
+const URL = "https://api.football-data.org/v4/matches";
+
+const response = fetch(URL, {
+  headers: {
+    "X-Auth-Token": "5366492db9d34e64bd0bd57c503d82ed",
+  },
+})
+  // const response = fetch(URL, {
+  //   method: "GET",
+  //   headers: {
+  //     "X-Auth-Token": "5366492db9d34e64bd0bd57c503d82ed",
+  //     "X-Unfold-Goals": "true",
+  //     "Accept-Encoding": "gzip, deflate, br",
+  //     Connection: "keep-alive",
+  //     "Content-Type": "application/json",
+  //   },
+  // })
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => data);
+
+console.log(response);
