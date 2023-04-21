78405def6dbb4bc57428674fd3c714b56cc4d1c9
aleksy.szary@gmail.com <aleksy.szary@gmail.com>
2023-04-21 17:05:08 +0700

    Делает правки метода

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ async function render() {
     headers: {
       "X-Auth-Token": "5366492db9d34e64bd0bd57c503d82ed",
     },
-  });
+  })
 
   let json = await res.json();
 
@@ -16,13 +16,23 @@ async function render() {
     return json.resultSet.competitions.split(",");
   }
 
+  function formatDate(value = "") {
+    const date = new Date(value);
+    const hours =
+      date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+    const minutes =
+      date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+
+    return `${hours}:${minutes}`;
+  }
+
   function createMatches(matches) {
     let res = "";
 
     matches.forEach((match) => {
       res += `<li>
     <article class="competitionList__matchWrapper">
-      <span>${match.utcDate}</span>
+      <span>${formatDate(match.utcDate)}</span>
       <img src="${match.homeTeam.crest}" width="30" height="30" alt="Логотип ${match.homeTeam.name}" />
       <span>${match.homeTeam.name}</span>
       <span>${match.score.fullTime.home}:${match.score.fullTime.away}</span>
